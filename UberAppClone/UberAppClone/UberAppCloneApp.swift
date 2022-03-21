47c4cfe3e74b4056761d6d91923ec6fcc91c4b25
brightsummit.co <naseemoyebola@gmail.com>
2022-03-21 11:36:30 +0100

    fix: Build error

--- UberAppClone/UberAppClone/UberAppCloneApp.swift
+++ UberAppClone/UberAppClone/UberAppCloneApp.swift
@@ -6,15 +6,21 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct UberAppCloneApp: App {
-    let persistenceController = PersistenceController.shared
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
+  
+  class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      FirebaseApp.configure()
+      return true
+    }
+  }
 }
